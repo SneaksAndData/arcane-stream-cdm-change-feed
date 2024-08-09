6342d3daf70a625bdb5a822da6f415445798a408
adelinag08 <adma@ecco.com>
2024-08-09 11:21:09 +0200

    debug

--- src/Program.cs
+++ src/Program.cs
@@ -16,6 +16,7 @@ Log.Logger = DefaultLoggingProvider.CreateBootstrapLogger(nameof(Arcane));
 int exitCode;
 try
 {
+    Console.WriteLine(AppDomain.CurrentDomain.FriendlyName.ToUpperInvariant());
     exitCode = await Host.CreateDefaultBuilder(args)
         .AddDatadogLogging((_, _, configuration) => configuration.EnrichWithCustomProperties().WriteTo.Console())
         .ConfigureRequiredServices(services =>
