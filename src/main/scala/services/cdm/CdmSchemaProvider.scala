a50dbfc2051b3b9fbc5180f95168dbbabb75d913
Vitalii Savitskii <visa@ecco.com>
2025-01-03 16:50:55 +0100

    Implement the requestChanges method

--- src/main/scala/services/cdm/CdmSchemaProvider.scala
+++ src/main/scala/services/cdm/CdmSchemaProvider.scala
@@ -1,11 +1,7 @@
 package com.sneaksanddata.arcane.cdm_change_feed
 package services.cdm
 
-import models.app.CdmStreamContext
-import services.graph_builder.{BackfillDataGraphBuilder, VersionedDataGraphBuilder}
-
 import com.sneaksanddata.arcane.framework.models.ArcaneSchema
-import com.sneaksanddata.arcane.framework.models.app.StreamContext
 import com.sneaksanddata.arcane.framework.models.cdm.{SimpleCdmEntity, SimpleCdmModel, given_Conversion_SimpleCdmEntity_ArcaneSchema}
 import com.sneaksanddata.arcane.framework.services.base.SchemaProvider
 import com.sneaksanddata.arcane.framework.services.cdm.CdmTableSettings
@@ -15,17 +11,22 @@ import zio.{ZIO, ZLayer}
 
 import scala.concurrent.Future
 
-class CdmSchemaProvider(azureBlobStorageReader: AzureBlobStorageReader, tableLocation: String, tableName: String) extends SchemaProvider[ArcaneSchema]:
+class CdmSchemaProvider(azureBlobStorageReader: AzureBlobStorageReader, tableLocation: String, tableName: String)
+  extends SchemaProvider[ArcaneSchema]:
+
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
-  override def getSchema: Future[SchemaType] = SimpleCdmModel(tableLocation, azureBlobStorageReader).flatMap(toArcaneSchema)
+  override def getSchema: Future[SchemaType] = getEntity.flatMap(toArcaneSchema)
+
+  def getEntity: Future[SimpleCdmEntity] =
+    SimpleCdmModel(tableLocation, azureBlobStorageReader).flatMap(_.entities.find(_.name == tableName) match
+      case None => Future.failed(new Exception(s"Table $tableName not found in model $tableLocation"))
+      case Some(entity) => Future.successful(entity)
+    )
 
   override def empty: SchemaType = ArcaneSchema.empty()
   
-  private def toArcaneSchema(simpleCdmModel: SimpleCdmModel): Future[ArcaneSchema] =
-    simpleCdmModel.entities.find(_.name == tableName) match
-      case None => Future.failed(new Exception(s"Table $tableName not found in model $tableLocation"))
-      case Some(entity) => Future.successful(entity)
+  private def toArcaneSchema(simpleCdmModel: SimpleCdmEntity): Future[ArcaneSchema] = Future.successful(simpleCdmModel)
 
 object CdmSchemaProvider:
   
